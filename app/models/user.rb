01bce838ed5cf53ce4b00e8b0dac3641ac87d8cb
板本　耕輔 <kosukeitamoto@itamotokosukenoMacBook-Pro.local>
2021-03-09 14:48:35 +0900

    sns認証した際、編集ページにパスワード変更欄を非表示にした  #32

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,19 @@ class User < ApplicationRecord
     stocks.exists?(post_id: post.id)
   end
 
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
   # ----- 不要コード ------------------------------------------------------
 
   # 与えられた文字列のハッシュ値を返す
