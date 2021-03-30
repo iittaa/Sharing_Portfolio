336833cfb151caa14ffe63ab73150ea020d0053e
板本　耕輔 <kosukeitamoto@itamotokosukenoMacBook-Pro.local>
2021-03-30 17:19:25 +0900

    capistrano修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,16 +44,27 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  # desc 'Create database'
-  # task :db_create do
-  #   on roles(:db) do |host|
-  #     with rails_env: fetch(:rails_env) do
-  #       within current_path do
-  #         execute :bundle, :exec, :rake, 'db:create'
-  #       end
-  #     end
-  #   end
-  # end
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
 
   after :publishing, :restart
 
